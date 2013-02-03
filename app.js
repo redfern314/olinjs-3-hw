9953f595d6f22a72ea9a0f5464beb89d05ef6121
Derek Redfern <redfern.derek@gmail.com>
2013-02-02 21:07:00 -0500

    Added Procfile! I found the bug!

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOLAB_URI||'localhost');
 //mongoose.connect(process.env.MONGOLAB_URI||'localhost');
 
-// var models = require("./models/models");
-// var ingredient = require("./routes/ingredient");
-// var neworder = require("./routes/neworder");
-// var order = require("./routes/order");
+var models = require("./models/models");
+var ingredient = require("./routes/ingredient");
+var neworder = require("./routes/neworder");
+var order = require("./routes/order");
 
 var app = express();
 
@@ -37,16 +37,13 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', function(req, res){
-  res.render('index', { title: 'Express' });
-});
-// app.get('/', order.home);
-// app.get('/ingredient/new', ingredient.new);
-// app.post('/ingredient/new', ingredient.new_post);
-// app.get('/order/new', neworder.new);
-// app.post('/order/new', neworder.new_post);
-// app.get('/order', order.home);
-// app.post('/order', order.home_post);
+app.get('/', order.home);
+app.get('/ingredient/new', ingredient.new);
+app.post('/ingredient/new', ingredient.new_post);
+app.get('/order/new', neworder.new);
+app.post('/order/new', neworder.new_post);
+app.get('/order', order.home);
+app.post('/order', order.home_post);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
